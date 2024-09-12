bc4c75d697b8a0e667fcaac456cd98df0c569b0d
stuntmanmike87 <mich.perigaud@gmail.com>
2024-09-12 10:35:21 +0200

    Unused Rector rule

--- rector.php
+++ rector.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\CodeQuality\Rector\Property\OrderByKeyToClassConstRector;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
@@ -59,6 +58,4 @@ return static function (RectorConfig $rectorConfig): void {
         ]);
 
     $rectorConfig->importShortClasses(false);
-
-    $rectorConfig->skip([OrderByKeyToClassConstRector::class,]);
 };
